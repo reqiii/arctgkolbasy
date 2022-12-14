6082892449e5adf42aba1feeed9532c36e4e21c1
rales <r.aleshin97@gmail.com>
2022-12-14 19:29:03 +0300

    Initial commit

---
+++ src/main/kotlin/org/arctgkolbasy/ArctgkolbasyApplication.kt
@@ -0,0 +1,11 @@
+package org.arctgkolbasy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ArctgkolbasyApplication
+
+fun main(args: Array<String>) {
+	runApplication<ArctgkolbasyApplication>(*args)
+}
---
+++ src/test/kotlin/org/arctgkolbasy/ArctgkolbasyApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.arctgkolbasy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ArctgkolbasyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
